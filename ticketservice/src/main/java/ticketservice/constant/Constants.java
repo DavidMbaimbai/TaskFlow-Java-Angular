c49ffc471a146a13c8c3011d7ebd552b0b628d22
David Mbaimbai <dmbaimbai@csod.com>
2025-04-07 18:11:21 +0200

    Added new services

--- ticketservice/src/main/java/ticketservice/constant/Constants.java
+++ ticketservice/src/main/java/ticketservice/constant/Constants.java
@@ -2,6 +2,6 @@ package ticketservice.constant;
 
 public class Constants {
     public static final String FILE_NAME_HEADER = "File=Name";
-    public static final String GET_ARRAYS_LLC = "Get Arrays, LLC.";
+    public static final String TASK_FLOWER_LLC = "Task Flower, LLC.";
     public static final String PHOTO_DIRECTORY = System.getProperty("user.home") + "/Downloads/uploads/";
 }
